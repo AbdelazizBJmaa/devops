5540e1b7d92e2be0ba353666f543df35ad68f384
vbox <vbox@vbox-VirtualBox>
2024-12-17 19:59:50 +0100

    new mvn

--- build/mvn.sh
+++ build/mvn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd  /home/vbox/jenkins/pipeline/demo
+cd  /home/vbox/jenkins/pipeline/demo && docker run --rm -v $PWD:/app -v /root/.m2:/root/.m2 -w /app maven $@
 docker run --rm -v $PWD:/app -v /root/.m2:/root/.m2 -w /app maven $@ 
