4e8930fe9e22f86c803b958ddc0c627557526527
vbox <vbox@vbox-VirtualBox>
2024-12-17 22:37:29 +0100

    new path

--- build/mvn.sh
+++ build/mvn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd  demo && /usr/bin/docker run --rm -v $PWD:/app -v /root/.m2:/root/.m2 -w /app maven $@
+cd  demo && ./usr/bin/docker run --rm -v $PWD:/app -v /root/.m2:/root/.m2 -w /app maven $@
